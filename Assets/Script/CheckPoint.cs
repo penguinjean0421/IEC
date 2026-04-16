cd03411594b901ba03ac9ab48740b92d6d1fec4a
penguinjean0421 <penguinjean0421@gmail.com>
2026-04-16 10:48:07 +0900

    Edit Check Points
    - 체크포인트, 데드존 로직 분리
    - To Do : 리스폰, 체크포인트 로직 분리

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -1,42 +1,34 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
     public Color defaultColor = Color.white;
-    private Vector3 vectorPoint;
-
+    private Vector3 vectorPoint; // 이거 데드존으로 전송해야지
     private Renderer lastCheckpointRenderer;
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == 11)
-        {
-            transform.position = vectorPoint;
-        }
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("CheckPoint"))
         {
-            // 1. 이전에 저장된 체크포인트가 있다면 색상을 원래대로 복구
+            /* 이전에 저장된 체크포인트가 있다면 색상을 원래대로 복구*/
             if (lastCheckpointRenderer != null) { lastCheckpointRenderer.material.color = defaultColor; }
 
-            // 2. 현재 체크포인트 위치 저장
-            vectorPoint = transform.position;
+            /* 현재 체크포인트 위치 저장*/
+            vectorPoint = other.transform.position;
             Debug.Log($"Check Point : {vectorPoint}");
 
-            // 3. 현재 체크포인트의 렌더러를 가져와서 녹색으로 변경
+            /*현재 체크포인트의 렌더러를 가져와서 녹색으로 변경*/
             Renderer currentRenderer = other.gameObject.GetComponent<Renderer>();
-            if (currentRenderer != null)
-            {
-                currentRenderer.material.color = Color.green;
+            currentRenderer.material.color = Color.green;
 
-                // 4. 이제 이 체크포인트가 '마지막(이전) 체크포인트'가 됩니다.
-                lastCheckpointRenderer = currentRenderer;
-            }
+            /* 4. 이제 이 체크포인트가 마지막(이전) 체크포인트로 설정 */
+            lastCheckpointRenderer = currentRenderer;
         }
+    }
+
+    public void ReSpawnPlayer(GameObject player)
+    {
+        player.transform.position = vectorPoint;
     }
 
 }
---
+++ Assets/Script/DeadZone.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 체크포인트에서 부활
+            other.GetComponent<CheckPoint>().ReSpawnPlayer(other.gameObject);
+        }
+    }
+}
