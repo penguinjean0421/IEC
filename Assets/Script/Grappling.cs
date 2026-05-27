24fac3362569cca2ae707b064fae055b1c7edb18
Mkqwer <meebk2@naver.com>
2026-05-28 01:09:52 +0900

    Beta Version1

--- Assets/Script/Grappling.cs
+++ Assets/Script/Grappling.cs
@@ -43,16 +43,10 @@ public class Grappling : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(grappleKey) && grapplingCdTimer <= 0)
-            StartCoroutine(TryGrapple());
+            StartGrapple();
 
         if (grapplingCdTimer > 0)
             grapplingCdTimer -= Time.deltaTime;
-    }
-
-    private IEnumerator TryGrapple()
-    {
-        yield return new WaitForEndOfFrame();
-        StartGrapple();
     }
 
     private void LateUpdate()
@@ -66,11 +60,8 @@ public class Grappling : MonoBehaviour
         if (grapplingCdTimer > 0) return;
 
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-
         RaycastHit hit;
         bool hitSomething = Physics.Raycast(ray.origin, ray.direction, out hit, maxGrappleDistance, whatIsGrappleable);
-
-        Debug.DrawRay(ray.origin, ray.direction * maxGrappleDistance, hitSomething ? Color.green : Color.red, 2f);
 
         if (!hitSomething)
         {
@@ -84,7 +75,10 @@ public class Grappling : MonoBehaviour
         pm.state = PlayerMovement.MovementState.grappling;
         pm.freeze = true;
 
-        Debug.Log("We hit: " + hit.collider.name);
+        // 🌟 그래플이 벽에 박히면 대시 횟수 재장전!
+        Dashing dashingScript = GetComponent<Dashing>();
+        if (dashingScript != null) dashingScript.ResetDashCount();
+
         grapplePoint = hit.point;
         Invoke(nameof(ExecuteGrapple), grappleDelayTime);
 
@@ -92,7 +86,6 @@ public class Grappling : MonoBehaviour
         lr.positionCount = animationQuality;
         lr.SetPosition(1, grapplePoint);
     }
-
 
     private void ExecuteGrapple()
     {
@@ -102,21 +95,21 @@ public class Grappling : MonoBehaviour
         Vector3 gravityUp = -pm.currentGravity.normalized;
         Vector3 lowestPoint = transform.position - gravityUp * 1f;
 
+        float distance = Vector3.Distance(transform.position, grapplePoint);
+        float dynamicOvershoot = overshootYAxis + (distance * 0.3f); 
+
         float grapplePointRelativeYPos = Vector3.Dot(grapplePoint - lowestPoint, gravityUp);
-        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+        float highestPointOnArc = grapplePointRelativeYPos + dynamicOvershoot;
 
-        if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
+        if (grapplePointRelativeYPos < 0) highestPointOnArc = dynamicOvershoot;
 
-        // 🌟 핵심 수정: 최종 도착지(grapplePoint) 자체에 Overshoot 높이(2)를 더해버립니다!
-        // 이제 12를 쏘면 14를 향해 날아가서 블럭 위로 예쁘게 안착합니다.
-        Vector3 finalDestination = grapplePoint + (gravityUp * overshootYAxis);
+        Vector3 finalDestination = grapplePoint + (gravityUp * 1.5f); 
 
-        // 기존의 grapplePoint 대신 finalDestination으로 날아가게 변경합니다.
         pm.JumpToPosition(finalDestination, highestPointOnArc);
 
         Invoke(nameof(StopGrapple), 1f);
     }
-   
+
     public void StopGrapple()
     {
         pm.freeze = false;
@@ -127,6 +120,9 @@ public class Grappling : MonoBehaviour
 
         lr.enabled = false;
         pm.state = PlayerMovement.MovementState.air;
+
+        // 🌟 그래플이 끝나고 공중에서 떨어질 때, 천천히 떨어지도록 감속 플래그 ON!
+        pm.isDroppingFromGrapple = true; 
     }
 
     public bool IsGrappling()
@@ -151,30 +147,190 @@ public class Grappling : MonoBehaviour
         for (int i = 0; i < animationQuality; i++)
         {
             float delta = i / (float)(animationQuality - 1);
-
             Vector3 offset = cam.up * Mathf.Sin(delta * waveFrequency) * waveAmplitude * curveValue;
-
             Vector3 targetPos = Vector3.Lerp(gunTip.position, grapplePoint, delta) + offset;
-
             lr.SetPosition(i, targetPos);
         }
-    } 
-    // private void ExecuteGrapple()
-    // {
-    //     pm.freeze = false;
-    //     pm.grappling = true;
-
-    //     Vector3 gravityUp = -pm.currentGravity.normalized;
-    //     Vector3 lowestPoint = transform.position - gravityUp * 1f;
-
-    //     float grapplePointRelativeYPos = Vector3.Dot(grapplePoint - lowestPoint, gravityUp);
-    //     float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
-
-    //     if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
-
-    //     pm.JumpToPosition(grapplePoint, highestPointOnArc);
-
-    //     Invoke(nameof(StopGrapple), 1f);
-    // }
-
+    }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Grappling : MonoBehaviour
+// {
+//     [Header("References")]
+//     private PlayerMovement pm;
+//     public Transform cam;
+//     public Transform gunTip;
+//     public LayerMask whatIsGrappleable;
+//     public LineRenderer lr;
+
+//     [Header("Grappling")]
+//     public float maxGrappleDistance;
+//     public float grappleDelayTime;
+//     public float overshootYAxis;
+
+//     private Vector3 grapplePoint;
+
+//     [Header("Cooldown")]
+//     public float grapplingCd;
+//     private float grapplingCdTimer;
+
+//     [Header("Input")]
+//     public KeyCode grappleKey = KeyCode.Mouse1;
+
+//     [Header("Rope Animation")]
+//     public int animationQuality = 50;
+//     public float waveAmplitude = 0.1f;
+//     public float waveFrequency = 15f;
+//     public float animationSpeed = 20f;
+//     public AnimationCurve waveCurve;
+//     private float animationTimer;
+
+//     private bool grappling;
+
+//     private void Start()
+//     {
+//         pm = GetComponent<PlayerMovement>();
+//     }
+
+//     private void Update()
+//     {
+//         if (Input.GetKeyDown(grappleKey) && grapplingCdTimer <= 0)
+//             StartCoroutine(TryGrapple());
+
+//         if (grapplingCdTimer > 0)
+//             grapplingCdTimer -= Time.deltaTime;
+//     }
+
+//     private IEnumerator TryGrapple()
+//     {
+//         yield return new WaitForEndOfFrame();
+//         StartGrapple();
+//     }
+
+//     private void LateUpdate()
+//     {
+//         if (grappling)
+//             DrawRopeAnimated();
+//     }
+
+//     private void StartGrapple()
+//     {
+//         if (grapplingCdTimer > 0) return;
+
+//         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+//         RaycastHit hit;
+//         bool hitSomething = Physics.Raycast(ray.origin, ray.direction, out hit, maxGrappleDistance, whatIsGrappleable);
+
+//         Debug.DrawRay(ray.origin, ray.direction * maxGrappleDistance, hitSomething ? Color.green : Color.red, 2f);
+
+//         if (!hitSomething)
+//         {
+//             Debug.Log("No Target: Grapple Canceled");
+//             return;
+//         }
+
+//         animationTimer = 0f;
+//         grappling = true;
+
+//         pm.state = PlayerMovement.MovementState.grappling;
+//         pm.freeze = true;
+
+//         Debug.Log("We hit: " + hit.collider.name);
+//         grapplePoint = hit.point;
+//         Invoke(nameof(ExecuteGrapple), grappleDelayTime);
+
+//         lr.enabled = true;
+//         lr.positionCount = animationQuality;
+//         lr.SetPosition(1, grapplePoint);
+//     }
+
+
+//     private void ExecuteGrapple()
+//     {
+//         pm.freeze = false;
+//         pm.grappling = true;
+
+//         Vector3 gravityUp = -pm.currentGravity.normalized;
+//         Vector3 lowestPoint = transform.position - gravityUp * 1f;
+
+//         float grapplePointRelativeYPos = Vector3.Dot(grapplePoint - lowestPoint, gravityUp);
+//         float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+
+//         if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
+
+//         // 🌟 핵심 수정: 최종 도착지(grapplePoint) 자체에 Overshoot 높이(2)를 더해버립니다!
+//         // 이제 12를 쏘면 14를 향해 날아가서 블럭 위로 예쁘게 안착합니다.
+//         Vector3 finalDestination = grapplePoint + (gravityUp * overshootYAxis);
+
+//         // 기존의 grapplePoint 대신 finalDestination으로 날아가게 변경합니다.
+//         pm.JumpToPosition(finalDestination, highestPointOnArc);
+
+//         Invoke(nameof(StopGrapple), 1f);
+//     }
+   
+//     public void StopGrapple()
+//     {
+//         pm.freeze = false;
+//         pm.grappling = false;
+//         grappling = false;
+
+//         grapplingCdTimer = grapplingCd;
+
+//         lr.enabled = false;
+//         pm.state = PlayerMovement.MovementState.air;
+//     }
+
+//     public bool IsGrappling()
+//     {
+//         return grappling;
+//     }
+
+//     public Vector3 GetGrapplePoint()
+//     {
+//         return grapplePoint;
+//     }
+
+//     void DrawRopeAnimated()
+//     {
+//         if (!grappling) return;
+
+//         animationTimer += Time.deltaTime * animationSpeed;
+//         lr.positionCount = animationQuality;
+
+//         float curveValue = waveCurve.Evaluate(animationTimer);
+
+//         for (int i = 0; i < animationQuality; i++)
+//         {
+//             float delta = i / (float)(animationQuality - 1);
+
+//             Vector3 offset = cam.up * Mathf.Sin(delta * waveFrequency) * waveAmplitude * curveValue;
+
+//             Vector3 targetPos = Vector3.Lerp(gunTip.position, grapplePoint, delta) + offset;
+
+//             lr.SetPosition(i, targetPos);
+//         }
+//     } 
+//     // private void ExecuteGrapple()
+//     // {
+//     //     pm.freeze = false;
+//     //     pm.grappling = true;
+
+//     //     Vector3 gravityUp = -pm.currentGravity.normalized;
+//     //     Vector3 lowestPoint = transform.position - gravityUp * 1f;
+
+//     //     float grapplePointRelativeYPos = Vector3.Dot(grapplePoint - lowestPoint, gravityUp);
+//     //     float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+
+//     //     if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
+
+//     //     pm.JumpToPosition(grapplePoint, highestPointOnArc);
+
+//     //     Invoke(nameof(StopGrapple), 1f);
+//     // }
+
+// }
